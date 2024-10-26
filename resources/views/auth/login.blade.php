50742a2d0eedacf5d7a55544869e16c35ff52f7e
deknaa <kdkrisnaaa@gmail.com>
2024-10-26 17:50:21 +0800

    feat(login): added basic UI for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,106 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <div class="flex max-h-screen w-full">
+
+        <div class="w-1/2">
+            <div class="bg-green-600 h-screen">
+                <div class="p-7 flex justify-end h-[20vh]">
+                    <div class="flex items-center justify-center">
+                        <a href="{{ url('/') }}"
+                            class="text-slate-200 hover:text-white border border-slate-200 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800 w-full">
+                            <div class="flex gap-2">
+                                <svg class="w-6 h-6 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+                                </svg>
+                                Back to website
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="flex items-center justify-end h-[80vh]">
+                    <img src="{{ asset('image/landingpage/doctor.png') }}" alt="" class="w-full h-full">
+                </div>
+            </div>
+        </div>
+
+        <div class="w-1/2 flex flex-col justify-center mt-10">
+            <div class="ml-48">
+                <h1 class="text-4xl font-extrabold">Health<span class="text-green-600">Care</span></h1>
+                <h2 class="text-lg mb-10">Please Enter Your Account Details</h2>
+                <form action="{{ route('login') }}" method="POST">
+                    @csrf
+                    
+                    {{-- Email Address --}}
+                    <div class="mb-6">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
+                        <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email address" required autofocus autocomplete="username" value="{{ old('email') }}"/>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                     {{-- <!-- Password -->
+                    <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full"
+                                        type="password"
+                                        name="password"
+                                        required autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div> --}}
+
+                    {{-- Password --}}
+                    <div class="mb-6">
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" id="password" name="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required autocomplete="current-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-start w-2/3">
+                        <div>
+                            <input id="remember_me" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" name="remember"/>
+                            <label for="remember_me" class="ms-2 me-36 text-sm font-medium text-gray-900 dark:text-gray-300">{{ __('Remember Me') }}</label>
+                        </div>
+                        <div>
+                            @if (Route::has('password.request'))    
+                                <a href="{{ route('password.request') }}" class=" text-sm text-blue-700 hover:underline dark:text-blue-500">{{ __('Forgot Password?') }}</a>
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="">
+                        <button type="submit" class="w-2/3 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-8">{{ __('Sign In') }}</button>
+                    </div>
+
+                    <div class="flex justify-center items-center gap-3 w-2/3 mt-2">
+                        <hr class="w-2/5">
+                        <p>Or</p>
+                        <hr class="w-2/5">
+                    </div>
+
+                    <div class="flex items-center justify-center w-2/3 mt-3">
+                        <button type="button" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
+                            <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
+                            <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
+                            </svg>
+                            Sign in with Google
+                        </button>
+                    </div>
+
+                    @if (Route::has('register'))
+                    <div class="flex items-center justify-end mt-10 w-2/3">
+                        <a href="{{ route('register') }}" class="ml-5 underline hover:text-blue-500">Create an account</a>
+                    </div>
+                    @endif
+                    
+                </form>
+            </div>
+        </div>
+
+    </div>
+
+    {{-- <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
@@ -38,7 +137,7 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-            @if(Route::has('register'))
+            @if (Route::has('register'))
                 <a href="{{ route('register') }}" class="ml-5">Register</a>
             @endif
 
@@ -46,5 +145,5 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
-    </form>
+    </form> --}}
 </x-guest-layout>
