c43cfda90c9c77433fbcfef1b5ed0149a694ef14
deknaa <kdkrisnaaa@gmail.com>
2025-06-15 16:41:43 +0800

    feat: added user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'user',
+            'username' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('User12345'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'username' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('Admin12345'),
+        ]);
+    }
+}
